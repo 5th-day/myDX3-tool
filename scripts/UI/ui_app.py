b2e033332fc828e77b742609da5e5f47aec9ab6e
5th-day <35966787+5th-day@users.noreply.github.com>
2023-05-11 23:21:30 +0900

    prepare flet framework

---
+++ scripts/UI/ui_app.py
@@ -0,0 +1,22 @@
+import flet as ft
+
+class App:
+    """ Character maker class """
+    def __init__(self) :
+        pass
+    def __del__(self) :
+        pass
+    @classmethod
+    def run(cls) :
+        pass
+
+def home(page: ft.page):
+        pass
+
+def run():
+    ft.app(target=home)
+    # ft.app(target=home, view=ft.WEB_BROWSER)
+
+
+if __name__ == "__main__":
+    run()
---
+++ scripts/character_maker.py
@@ -0,0 +1,11 @@
+# coding: UTF-8
+
+from UI import ui_app
+
+def main():
+    ui_app.run()
+    print()
+
+
+if __name__ == "__main__":
+    main()
